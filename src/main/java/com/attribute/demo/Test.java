d610d08760a6c9414539e05bfa78a5ac2167d0cc
wujie <572066415@qq.com>
2019-03-08 10:23:37 +0800

    6

---
+++ src/main/java/com/attribute/demo/Test.java
@@ -0,0 +1,9 @@
+package com.attribute.demo;
+
+import com.sun.org.apache.xerces.internal.util.SynchronizedSymbolTable;
+
+public class Test {
+    public static void main(String[] args) {
+        System.out.println("aaaa");
+    }
+}
