daa2aca61e46327891ccb49128969975a098c0c0
wujie <572066415@qq.com>
2019-03-15 16:42:58 +0800

    1

--- src/main/java/com/attribute/demo/Test.java
+++ src/main/java/com/attribute/demo/Test.java
@@ -4,6 +4,6 @@ import com.sun.org.apache.xerces.internal.util.SynchronizedSymbolTable;
 
 public class Test {
     public static void main(String[] args) {
-        System.out.println("2");
+        System.out.println("3");
     }
 }
